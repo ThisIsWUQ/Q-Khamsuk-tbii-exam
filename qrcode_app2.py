4d36f7af5dde9c5d1ce62b65c9a8b477ea944845
ThisIsWUQ <sahadsawatk@gmail.com>
2024-11-12 15:30:51 +0100

    add the app file

---
+++ qrcode_app2.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from qrcode_generator_app import generate_qrcode
+from decode_qrcode_page import decode_qrcode
+
+st.set_page_config(page_title="My QR Code App",
+                   page_icon=":white_check_mark:")
+
+options = ['Create QR Code', 'Decode QR Code', 'About Me']
+page_selection = st.sidebar.selectbox("Menu", options)
+
+st.write(page_selection)
+
+if page_selection == 'Create QR Code':
+    generate_qrcode()
+elif page_selection == 'Decode QR Code':
+    decode_qrcode()
+elif page_selection == 'About Me':
+    st.write("Hi, I am a cat")
